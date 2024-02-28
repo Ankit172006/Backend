475c7176f58e3d4b1da8b0d15dfb9a76a2b8a190
Karan <karantha289@gmail.com>
2024-02-28 10:59:05 +0530

    use params in backend

--- Second.js
+++ Second.js
@@ -1,21 +1,54 @@
 
+
 const express = require('express');
-const e = require('express');
 const app = express();
-const PORT = 5000;
 
-const student = express.Router();
-// app.use('/student', student);
-
-student.get('/profile/:start/:end', function (req, res) {
-	console.log( req.params['start']);
-    dispatchEvent   (params)
-
-	// console.log("Ending Page: ", req.params['end']);
-	res.send();
+app.get('/home',(req,res)=>{
+	res.send("This is Home page")
+})
+app.get('/instagram/:id',(req,res)=>{
+  console.log(req.params.id)
+  res.send(req.params.id)
+  console.log(req.query)
+  res.send(req.State)
 })
 
-app.listen(PORT, function (err) {
-	if (err) console.log(err);
-	console.log("Server listening on PORT", PORT);
-});
+app.listen(6060,()=>{
+    console.log("Server is start")
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const e = require('express');
+// const app = express();
+// const PORT = 9000;
+
+// const student = express.Router();
+// // app.use('/student', student);
+
+// student.get('/profile/:id', function (req, res) {
+// 	console.log( req.params.id);
+// 	res.send("Hello from Backend");
+// })
+
+// app.listen(PORT, function (err) {
+// 	if (err) console.log(err);
+// 	console.log("Server listening on PORT", PORT);
+// });
