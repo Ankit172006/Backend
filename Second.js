bd557577901dbbd4fcc32291e51c6f55e4a90a7d
Karan <karantha289@gmail.com>
2024-03-04 18:56:53 +0530

    add app.use in rexpress ja

--- Second.js
+++ Second.js
@@ -2,9 +2,23 @@
 
 const express = require('express');
 const app = express();
+app.use=express.json();
+ const data=((req,res,next)=>{
+  if(req.query.pass==="Strong@098!")
+  {
+    next();
+  }
+  else
+  {
+    console.log("Password Incorrect+")
+  }
+
+ })
 
 app.get('/home',(req,res)=>{
 	res.send("This is Home page")
+  // console.log(req.query.pass)
+
 })
 app.get('/instagram/:id',(req,res)=>{
   console.log(req.params.id)
@@ -12,7 +26,14 @@ app.get('/instagram/:id',(req,res)=>{
   console.log(req.query)
   res.send(req.State)
 })
-
+app.get("/password",data,(req,res)=>{
+  console.log("Login")
+  console.log("pass:*********")
+  res.send("Login !👍👍👍👍👍")
+})
+app.post('/postimg',(req,res)=>{
+  res.send("From post side")
+})
 app.listen(6060,()=>{
     console.log("Server is start")
  })
