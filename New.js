c3752dc52ca1e6a014ec2900c1ebe03c0861e7d0
Karan <karantha289@gmail.com>
2024-03-13 19:02:11 +0530

    read,update and delete data in mongodb

--- New.js
+++ New.js
@@ -13,12 +13,30 @@ const app = express();
   })
  const ankit =mongoose.model("tuf",nameSchema)
 
- const name = new ankit({
-    name:"karan",
-    password:"HP 82",
-    Monumber:85265935656946
-})
- name.save();
+//  const name = new ankit({
+//     name:"karan",
+//     password:"HP 82",
+//     Monumber:85265935656946
+// })
+ankit.findByIdAndUpdate('65eb031cd4ac33b81010df61')
+.then((value)=>{console.log(value)})
+//  name.save();
  app.listen(9999)
 
 //  boAt Rockerz 510 Super Extra Bass Bluetooth Headset
+
+// 1______
+// update data in mongodb
+
+// ankit.findByIdAndUpdate('65eb031cd4ac33b81010df61')
+// .then((value)=>{console.log(value)})
+
+// 2_____
+// delete data in mongodb
+// ankit.findByIdAndDelete('65eb031cd4ac33b81010df61')
+// .then((value)=>{console.log(value)})
+
+// 3_____
+// Read data in mongodb
+// ankit.findById('65eb031cd4ac33b81010df61')
+// .then((value)=>{console.log(value)})
